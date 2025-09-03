cb247a21bb0480d0557231f3670d26783cbfad73
Badsha012 <badshasheikh361@gmail.com>
2025-09-03 16:57:45 +0600

    five commit

---
+++ todo.js
@@ -0,0 +1,31 @@
+const toTodo=() =>{
+    const url="https://jsonplaceholder.typicode.com/todos"
+    fetch(url)
+    .then(res => res.json())
+    .then(data => dispalytodo(data));
+};
+const dispalytodo=(todos)=>{
+    // console.log(todos);
+    const todoContainer=document.getElementById("todo-container");
+    todoContainer.innerHTML="";
+
+    todos.forEach(todo=>{
+        console.log(todo);
+
+        const todoCard=document.createElement("div");
+        todoCard.innerHTML=`
+   
+        <div class= "todo-card">
+        <p>${todo.completed === true ?
+             `<i class="fa-solid fa-square-check"></i>`
+             : `<i class="fa-regular fa-square-check"></i>`}
+            </p>
+        <h4>${todo.title}</h4>
+        
+        </div>`
+        todoContainer.append(todoCard)
+});
+ //todoContainer.append(todoCard);
+
+};
+toTodo();
